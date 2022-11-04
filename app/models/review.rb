0c472c1281aa32455b4b4c082cf8544aab3376c8
Essyuge <ugeesther@gmail.com>
2022-11-04 09:34:45 +0300

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
-  
-end
+   # a review belongs to a game
+#    def game
+    # self is the review instance
+    # Game.find(self.game_id)
+    belongs_to :game
+  end
+
